08d12d9ccb3f31455ceb2568e75bbadc05e1ddfd
Nitin Balkrishna Shetye <shetye.nitin13@gmail.com>
2025-01-12 19:59:34 +0530

    string palindrome

---
+++ src/practicequestions150/PalindromeString_6.java
@@ -0,0 +1,13 @@
+package practicequestions150;
+
+import java.util.Scanner;
+
+public class PalindromeString_6 {
+    public static void main(String[] args) {
+        String str = "Nitin";
+
+        String strReversed = "";
+
+        
+    }
+}
