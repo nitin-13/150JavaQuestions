2652c6f9f6416e641bb33106fdf00a7e3919d1ee
Nitin Balkrishna Shetye <shetye.nitin13@gmail.com>
2025-01-12 14:31:04 +0000

    PalindromeString_6.java edited online with Bitbucket

--- src/practicequestions150/PalindromeString_6.java
+++ src/practicequestions150/PalindromeString_6.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class PalindromeString_6 {
     public static void main(String[] args) {
-        String str = "Nitin";
+        String str = "nitin";
 
         String strReversed = "";
 
