e16b28f85fa9a73d0dc11e0b585950d10de066b4
Nitin Balkrishna Shetye <shetye.nitin13@gmail.com>
2025-01-12 19:21:54 +0530

    6th Question-Palindrom Solution

--- src/practicequestions150/PalindromeNumber_6.java
+++ src/practicequestions150/PalindromeNumber_6.java
@@ -1,4 +1,30 @@
 package practicequestions150;
 
+import java.util.Scanner;
+
 public class PalindromeNumber_6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = in.nextInt();
+
+        isPalindrome(num);
+    }
+
+    static void isPalindrome(int num) {
+        int temp = num;
+        int reversed = 0;
+        while (num > 0) {
+            reversed = (reversed * 10) + (num % 10);
+            num /= 10;
+        }
+
+        if (reversed == temp) {
+            System.out.println("Palindrome Number");
+        } else {
+            System.out.println("Not a Palindrome Number");
+        }
+        ;
+    }
 }
